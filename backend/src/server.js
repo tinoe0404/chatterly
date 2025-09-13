c40426d7d2b6b3b5016d0b0a4f0b925c42809d83
tinoe0404 <tinoe044@gmail.com>
2025-09-13 12:22:12 +0200

    finish environment setup and setting up some APIs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -7,20 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/auth/signup", (req,res) => {
-    res.send("Signup endpoint");
-})
-
-app.get("/api/auth/login", (req,res) => {
-    res.send("Login endpoint");
-})
-
-app.get("/api/auth/logout", (req,res) => {
-    res.send("Logout endpoint");
-})
-
-app.get("/api/auth/update", (req,res) => {
-    res.send("Update endpoint");
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(3000, () => console.log("Server running on port " + PORT));
