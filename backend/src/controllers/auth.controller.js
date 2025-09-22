dfdc3e0b1e1ece793c209910c8f4c03a6c98d735
tinoe0404 <tinoe044@gmail.com>
2025-09-22 15:34:49 +0200

    login and logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,3 +70,36 @@ export const signup = async (req,res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.error("Error during login controller:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const logout =(_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({ message: "Logged out successfully" });
+};
