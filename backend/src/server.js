b8da0009510348bdddca274eca667dc748f5dc13
tinoe0404 <tinoe044@gmail.com>
2025-09-23 14:13:42 +0200

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
  
 app.use(express.json());  //req.body
+app.use(cookieParser());
   
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
