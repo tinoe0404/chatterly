191c225dbfca62e3c3779a17827beaf3ba847e7b
tinoe0404 <tinoe044@gmail.com>
2025-09-25 09:43:32 +0200

    message-routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -42,6 +42,16 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!text && !image) {
+      return res.status(400).json({ error: "Message text or image is required" });
+    }
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({ error: "Cannot send message to yourself" });
+    }
+    const receiverExists = await User.exists({ _id: receiverId});
+    if (!receiverExists) {
+      return res.status(404).json({ error: "Receiver not found" });
+    }
     let imageUrl;
     if (image) {
       // Upload base64 image to Cloudinary
